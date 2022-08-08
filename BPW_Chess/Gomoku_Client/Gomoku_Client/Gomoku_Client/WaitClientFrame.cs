bc991b8fb0dad7718b827f935fcf595611e91373
zzzyz7 <zhengbill7@gmail.com>
2022-08-08 10:11:48 -0700

    BPW_CHESS

---
+++ BPW_Chess/Gomoku_Client/Gomoku_Client/Gomoku_Client/WaitClientFrame.cs
@@ -0,0 +1,58 @@
+﻿using Gomoku_Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    public partial class WaitClientFrame : Form
+    {
+        public WaitClientFrame()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CloseFrame()
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private delegate void CloseFrameDG();
+
+        public void CallCloseFrameDG()
+        {
+            this.Invoke(new CloseFrameDG(CloseFrame));
+        }
+
+        private void ShowMessageDialog(string text)
+        {
+            MessageBox.Show(text);
+        }
+
+        private delegate void ShowMessageDialogDG(string text);
+
+        public void CallShowMessageDialogDG(string text)
+        {
+            this.Invoke(new ShowMessageDialogDG(ShowMessageDialog), text);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void StartGameButton_Click(object sender, EventArgs e)
+        {
+            // 点击游戏后，向服务器发起开始游戏请求
+            Configuration.client.SendToServer("StartGame,"+Configuration.playerID.ToString());
+        }
+    }
+}
